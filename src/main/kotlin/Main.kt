8cf91998e05c54790c37368c3a3773c2405cedb3
Sabelo Thabede <sabelothabede01@gmail.com>
2025-04-30 11:38:35 +0200

    Commit Coroutines

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private fun log(msg:String) = println("[${Thread.currentThread().name}] $msg")
+
+fun main(){
+    networkRequest()
+}
+
+private fun networkRequest(){
+
+    GlobalScope.launch {
+        log("Making network request...")
+
+        for (i in 1..3){
+            delay(1000)
+            println("First: $i")
+        }
+        log("First network request made!")
+    }
+
+    GlobalScope.launch {
+        delay(500)
+        log("Making second network request...")
+
+        for (i in 1..3){
+            delay(1000)
+            println("Second: $i")
+        }
+        log("Second network request made!")
+    }
+    Thread.sleep(4000)
+
+    log("Done")
+
+
+
+}
